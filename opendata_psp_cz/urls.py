794adffe83406aa5334d8a5f5de0ff4f5c9ab135
Libor Nenadál <libor.nenadal@gmail.com>
2012-10-15 18:20:42 +0200

    Data browsing in HTML mode is now working.

--- opendata_psp_cz/urls.py
+++ opendata_psp_cz/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+import djangorestframework.urls
 import api.urls
 
 urlpatterns = patterns('',
@@ -15,6 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^djangorestframework/', include(djangorestframework.urls, namespace='djangorestframework')),
     url(r'^api/', include(api.urls)),
 )
